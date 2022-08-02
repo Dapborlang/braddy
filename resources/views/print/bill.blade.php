d15bbcb326ee3060020a218e02b9164db0f4f169
Dapbor <dapbor88@gmail.com>
2022-08-02 10:35:56 +0530

    added new bill

--- resources/views/print/bill.blade.php
+++ resources/views/print/bill.blade.php
@@ -16,7 +16,8 @@
         @php 
             $words=new NumberFormatter("en_IN", NumberFormatter::SPELLOUT);
             $figure=new NumberFormatter("en_IN",NumberFormatter::CURRENCY);
-            $figure->setAttribute(\NumberFormatter::MAX_FRACTION_DIGITS, 0);
+			$figure->setSymbol(NumberFormatter::CURRENCY_SYMBOL, '');
+            $figure->setAttribute(\NumberFormatter::MAX_FRACTION_DIGITS, 2);
         @endphp
    	<table border="1" width="100%" id="table2">
 	   	<thead>
@@ -35,11 +36,11 @@
 	   					</tr>
 	   					<tr>
 	   						<td style="text-align: left;"><span style="font-size: 12px;">Bill No: {{$bill->bill_no}}</span></td>
-	   						<td style="text-align: right;><span style="font-size: 12px;">Bill Date: {{$bill->bill_date}}</span></td>
+	   						<td style="text-align: right;><span style="font-size: 12px;">Bill Date: {{date("d/m/Y", strtotime($bill->bill_date))}}</span></td>
 	   					</tr>
 	   					<tr>
 	   						<td style="text-align: left;"><span style="font-size: 12px;">Order No: {{$bill->order_no}}</span></td>
-	   						<td style="text-align: right;><span style="font-size: 12px;">Order Date: {{$bill->order_date}}</span></td>
+	   						<td style="text-align: right;><span style="font-size: 12px;">Order Date: {{date("d/m/Y", strtotime($bill->order_date))}}</span></td>
 	   					</tr>
 	   				</table>
 	   			</th>
@@ -63,20 +64,29 @@
                     <td style="text-align: right;">{{round($item->rate - ($item->rate*$item->gst)/(100+$item->gst), 2)}}</td>
                     <td style="text-align: center;">{{$item->quantity}}</td>
                     <td style="text-align: center;">{{$item->gst}}</td>
-                    <td style="text-align: right;">{{($item->rate * $item->quantity)-(round($item->rate - ($item->rate*$item->gst)/(100+$item->gst), 2)*$item->quantity)}}</td>
-                    <td style="text-align: right;">{{$item->rate * $item->quantity}} @php $total[]=$item->rate * $item->quantity @endphp</td>
+                    <td style="text-align: right;">
+						{{$figure->format(($item->rate * $item->quantity)-(round($item->rate - ($item->rate*$item->gst)/(100+$item->gst), 2)*$item->quantity))}}
+						@php $gst[]=($item->rate * $item->quantity)-(round($item->rate - ($item->rate*$item->gst)/(100+$item->gst), 2)*$item->quantity) @endphp
+					</td>
+                    <td style="text-align: right;">{{$figure->format($item->rate * $item->quantity)}} @php $total[]=$item->rate * $item->quantity @endphp</td>
                 </tr>
             @endforeach
         @else
-            @foreach($bill->BillDetail as $item)
+			@foreach($bill->BillDetail as $item)
                 <tr>
                     <td style="text-align: center;">{{ $loop->iteration }}</td>
                     <td>{{$item->particular}}</td>
-                    <td style="text-align: right;">{{$item->rate}}</td>
+                    <td style="text-align: right;">{{round($item->rate, 2)}}</td>
                     <td style="text-align: center;">{{$item->quantity}}</td>
                     <td style="text-align: center;">{{$item->gst}}</td>
-                    <td style="text-align: right;"></td>
-                    <td style="text-align: right;"></td>
+                    <td style="text-align: right;">
+						{{$figure->format($item->rate * ($item->gst/100))}}
+						@php $gst[]=round($item->rate * ($item->gst/100)) @endphp
+					</td>
+                    <td style="text-align: right;">
+					{{$figure->format(($item->rate * $item->quantity)+($item->rate * ($item->gst/100)))}} 
+					@php $total[]=($item->rate * $item->quantity)+($item->rate * ($item->gst/100)) @endphp
+				</td>
                 </tr>
             @endforeach
         @endif
@@ -86,8 +96,8 @@
   				<td></td>
   				<td></td>
   				<td style="text-align: right; font-size: 15px; font-weight: bold;">Total</td>
-  				<td style="text-align: right; font-size: 15px; font-weight: bold;"></td>
-  				<td style="text-align: right; font-size: 15px; font-weight: bold;">{{array_sum($total)}}</td>
+  				<td style="text-align: right; font-size: 15px; font-weight: bold;">{{$figure->format(array_sum($gst))}}</td>
+  				<td style="text-align: right; font-size: 15px; font-weight: bold;">{{$figure->format(array_sum($total))}}</td>
   			</tr>
 		</tbody>
 		<tfoot>
@@ -98,22 +108,24 @@
 		    </tr>
 		    <tr>
 		    	<td colspan="4" style="text-align: right; font-size: 15px; font-weight: bold;">Taxable Value</td>
-		    	<td style="text-align: right; font-size: 15px; font-weight: bold;"></td>
+		    	<td style="text-align: right; font-size: 15px; font-weight: bold;">
+				{{$figure->format(array_sum($total)-array_sum($gst))}}
+				</td>
 		    </tr>
 		    <tr>
 		    	<td colspan="4" style="text-align: right; font-size: 15px; font-weight: bold;">SGST</td>
-		    	<td style="text-align: right; font-size: 15px; font-weight: bold;"></td>
+		    	<td style="text-align: right; font-size: 15px; font-weight: bold;">{{$figure->format(array_sum($gst)/2)}}</td>
 		    </tr>
 		    <tr>
 		    	<td colspan="4" style="text-align: right; font-size: 15px; font-weight: bold;">CGST</td>
-		    	<td style="text-align: right; font-size: 15px; font-weight: bold;"></td>
+		    	<td style="text-align: right; font-size: 15px; font-weight: bold;">{{$figure->format(array_sum($gst)/2)}}</td>
 		    </tr>
 		    <tr>
 		    	<td colspan="4" style="text-align: right; font-size: 15px; font-weight: bold;">Total</td>
-		    	<td style="text-align: right; font-size: 15px; font-weight: bold;"></td>
+		    	<td style="text-align: right; font-size: 15px; font-weight: bold;">{{$figure->format(array_sum($total))}}</td>
 		    </tr>
 		    <tr>
-		    	<td colspan="5" style="text-align: center; font-size: 15px; font-weight: bold;">(Rupees {{$words->format(array_sum($total))}}) only.</td>
+		    	<td colspan="5" style="text-align: center; font-size: 15px; font-weight: bold;">(Rupees {{ucwords($words->format(array_sum($total)))}}) only.</td>
 		    </tr>
 		</tfoot>
 	</table>
